f22f73090c1b239d11e201b279345671d915887a
Ellie Kobak <elliekobak@s0h029.goosehollow.ptp>
2023-08-25 10:49:58 -0700

    C++ file added

---
+++ src/random.cpp
@@ -0,0 +1,19 @@
+/*
+* random file to test
+* MeerCat PR 
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+class Solution {
+public:
+    bool isPalindrome(int x) {
+    
+
+
+    }
+};
